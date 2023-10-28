d5c36537d7eeb558c12dd51c46162053020b514d
Iyanuoluwa Ogundipe <n0xc0d3r@protonmail.com>
2023-10-28 07:56:02 +0100

    updated returned welcome mesage

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def index(path):
-    return 'Welcome to ABCCTF 2023'
+    return 'Welcome developers to the ABCCTF 2023 competition for developers!'
 
 if __name__ == '__main__':
     app.run()
